d6eef6069bf64f08380469b54a8149e640336cd3
yoshiking <yoshiking0711@gmail.com>
2019-11-26 18:13:50 +0900

    add magit

--- init.el
+++ init.el
@@ -95,7 +95,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (dracula-theme company-go go-mode js2-mode company-tern markdown-preview-mode solarized-theme flycheck counsel company-jedi company-irony))))
+    (magit dracula-theme company-go go-mode js2-mode company-tern markdown-preview-mode solarized-theme flycheck counsel company-jedi company-irony))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -127,3 +127,6 @@
 ;; company-go
 (require 'company-go)
 (add-hook 'go-mode-hook 'company-mode)
+
+;; magit
+(global-set-key (kbd "C-x g") `magit-status)
