b7a0d3375318d17441d38f625dc284d2edb080c8
yoshiking <yoshiking0711@gmail.com>
2020-05-29 20:12:09 +0900

    add org-mode

--- init.el
+++ init.el
@@ -229,3 +229,34 @@
 (defun jpk/sage-mode-hook ()
   (company-mode -1))
 (add-hook 'sage-shell-mode-hook 'jpk/sage-mode-hook)
+
+;; org
+(require 'org)
+(require 'org-tempo)
+(setq org-log-done 'time)
+(setq org-use-speed-commands t)
+(setq org-src-fontify-natively t)
+(setq org-agenda-files '("~/Dropbox/emacs/org/task.org"))
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("e" "Experiment" entry (file+headline "~/Dropbox/emacs/org/experiment.org" "Experiment")
+	 "* %? %U %i
+#+BEGIN_SRC emacs-lisp
+
+#+END_SRC")
+	("i" "Idea" entry (file+headline "~/Dropbox/emacs/org/idea.org" "Idea")
+	 "* %? %U %i")
+	("r" "Remember" entry (file+headline "~/Dropbox/emacs/org/remember.org" "Remember")
+	 "* %? %U %i")
+	("m" "Memo" entry (file+headline "~/Dropbox/emacs/org/memo.org" "Memo")
+	 "* %? %U %i")
+	("a" "Archives" entry (file+headline "~/Dropbox/emacs/org/archives.org" "Archives")
+	 "* %? %U %i")
+	("t" "Task" entry (file+headline "~/Dropbox/emacs/org/task.org" "Task")
+	 "** TODO %? \n   SCHEDULED: %^t \n")))
+(setq org-refile-targets
+      (quote (("~/Dropbox/emacs/org/archives.org" :level . 1)
+	      ("~/Dropbox/emacs/org/remember.org" :level . 1)
+	      ("~/Dropbox/emacs/org/memo.org" :level . 1)
+	      ("~/Dropbox/emacs/org/task.org" :level . 1))))
