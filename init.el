9a6651a65d650ea417d307caaf4865a0b18cd1fc
yoshiking <yoshiking0711@gmail.com>
2020-09-17 11:14:11 +0900

    Fix

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 
 ;;; Code:
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
