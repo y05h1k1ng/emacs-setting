af0038f34d0e8df047d4cc6cecb02c6071591b59
yoshiking <yoshiking0711@gmail.com>
2021-05-12 15:38:39 +0900

    Add windows mozc setting

--- init.el
+++ init.el
@@ -230,13 +230,108 @@
 		   (call-interactively 'org-agenda)))
   )
 
+;; mozc-setting
+(require 'mozc-im)
+(require 'mozc-popup)
+;;(require 'mozc-cursor-color)
+(require 'wdired)
+
+(setq default-input-method "japanese-mozc-im")
+
+;; popupスタイル を使用する
+(setq mozc-candidate-style 'popup)
+
+;; カーソルカラーを設定する
+;;(setq mozc-cursor-color-alist '((direct        . "red")
+;;                                (read-only     . "yellow")
+;;                                (hiragana      . "green")
+;;                                (full-katakana . "goldenrod")
+;;                                (half-ascii    . "dark orchid")
+;;                                (full-ascii    . "orchid")
+;;                                (half-katakana . "dark goldenrod")))
+
+;; カーソルの点滅を OFF にする
+(blink-cursor-mode 0)
+
+(defun enable-input-method (&optional arg interactive)
+  (interactive "P\np")
+  (if (not current-input-method)
+      (toggle-input-method arg interactive)))
+
+(defun disable-input-method (&optional arg interactive)
+  (interactive "P\np")
+  (if current-input-method
+      (toggle-input-method arg interactive)))
+
+(defun isearch-enable-input-method ()
+  (interactive)
+  (if (not current-input-method)
+      (isearch-toggle-input-method)
+    (cl-letf (((symbol-function 'toggle-input-method)
+               (symbol-function 'ignore)))
+      (isearch-toggle-input-method))))
+
+(defun isearch-disable-input-method ()
+  (interactive)
+  (if current-input-method
+      (isearch-toggle-input-method)
+    (cl-letf (((symbol-function 'toggle-input-method)
+               (symbol-function 'ignore)))
+      (isearch-toggle-input-method))))
+
+;; IME をトグルするキー設定
+(global-set-key (kbd "<zenkaku-hankaku>") 'toggle-input-method)
+(define-key isearch-mode-map (kbd "<zenkaku-hankaku>") 'isearch-toggle-input-method)
+(define-key wdired-mode-map (kbd "<zenkaku-hankaku>") 'toggle-input-method)
+
+;; IME を無効にするキー設定
+(global-set-key (kbd "C-<f1>") 'disable-input-method)
+(define-key isearch-mode-map (kbd "C-<f1>") 'isearch-disable-input-method)
+(define-key wdired-mode-map (kbd "C-<f1>") 'disable-input-method)
+
+;; (global-set-key (kbd "C-j") 'disable-input-method)
+;; (define-key isearch-mode-map (kbd "C-j") 'isearch-disable-input-method)
+;; (define-key wdired-mode-map (kbd "C-j") 'disable-input-method)
+
+;; IME を有効にするキー設定
+(global-set-key (kbd "C-<f2>") 'enable-input-method)
+(define-key isearch-mode-map (kbd "C-<f2>") 'isearch-enable-input-method)
+(define-key wdired-mode-map (kbd "C-<f2>") 'enable-input-method)
+
+;; (global-set-key (kbd "C-o") 'enable-input-method)
+;; (define-key isearch-mode-map (kbd "C-o") 'isearch-enable-input-method)
+;; (define-key wdired-mode-map (kbd "C-o") 'enable-input-method)
+
+;; mozc-cursor-color を利用するための対策
+;;(defvar-local mozc-im-mode nil)
+;;(add-hook 'mozc-im-activate-hook (lambda () (setq mozc-im-mode t)))
+;;(add-hook 'mozc-im-deactivate-hook (lambda () (setq mozc-im-mode nil)))
+;;(advice-add 'mozc-cursor-color-update
+;;            :around (lambda (orig-fun &rest args)
+;;                      (let ((mozc-mode mozc-im-mode))
+;;                        (apply orig-fun args))))
+
+;; isearch を利用する前後で IME の状態を維持するための対策
+(add-hook 'isearch-mode-hook (lambda () (setq im-state mozc-im-mode)))
+(add-hook 'isearch-mode-end-hook
+          (lambda ()
+            (unless (eq im-state mozc-im-mode)
+              (if im-state
+                  (activate-input-method default-input-method)
+                (deactivate-input-method)))))
+
+;; wdired 終了時に IME を OFF にする
+(advice-add 'wdired-finish-edit
+            :after (lambda (&rest args)
+                     (deactivate-input-method)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(go-mode lsp-python-ms yasnippet wgrep web-mode undo-tree rainbow-delimiters org nyan-mode multiple-cursors markdown-mode magit flycheck emoji-cheat-sheet-plus eglot counsel company color-theme-sanityinc-tomorrow all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired ace-window)))
+   '(mozc-im mozc-popup go-mode lsp-python-ms yasnippet wgrep web-mode undo-tree rainbow-delimiters org nyan-mode multiple-cursors markdown-mode magit flycheck emoji-cheat-sheet-plus eglot counsel company color-theme-sanityinc-tomorrow all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
