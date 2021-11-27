11e21b5837b2e333bfb2086cf7e5d34f9c94d78c
yoshiking <yoshiking0711@gmail.com>
2021-11-27 21:23:05 +0900

    Add avy setting

---
+++ myconf/my_avy.el
@@ -0,0 +1,4 @@
+(global-set-key (kbd "C-'") 'avy-goto-char-2)
+(global-set-key (kbd "C-:") 'avy-goto-char-timer)
+(global-set-key (kbd "M-g f") 'avy-goto-line)
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
